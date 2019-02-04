21efb7e0d3e56f849f04659f5e36df0683786e23
Alexandra Anghel <alexandra@appticles.com>
2019-02-04 13:31:04 +0200

    Rename features predictions table, set date as primary key

--- ingestion/keywords_repo.py
+++ ingestion/keywords_repo.py
@@ -3,11 +3,11 @@ from cassandra.query import BatchStatement
 
 class KeywordsByCSVRepo:
 
-	def __init__(self, session):
-		self.session = session
+    def __init__(self, session):
+        self.session = session
 
-		insert_statement = """
-		INSERT INTO usi_csv_keywords_by_csv (
+        insert_statement = """
+		INSERT INTO usi_csv_features_raw_p (
 		csv_file_date,
 		group_id,
 		keyword_id,
@@ -19,15 +19,15 @@ class KeywordsByCSVRepo:
 		VALUES (?, ?, ?, ?, ?, ?, ?)
 		"""
 
-		self.prep_insert_statement = session.prepare(insert_statement)
+        self.prep_insert_statement = session.prepare(insert_statement)
 
-	def insert(self, values):
-		print(values)
-		self.session.execute(self.prep_insert_statement, values)
+    def insert(self, values):
+        print(values)
+        self.session.execute(self.prep_insert_statement, values)
 
-	def batch_insert(self, batch_values):
-		batch = BatchStatement()
-		for val_set in batch_values:
-			batch.add(self.prep_insert_statement, val_set)
+    def batch_insert(self, batch_values):
+        batch = BatchStatement()
+        for val_set in batch_values:
+            batch.add(self.prep_insert_statement, val_set)
 
-		self.session.execute(batch)
+        self.session.execute(batch)
