627e4ff50b4ddc7d4cef60f060df5ef9282343b8
Alexandra Anghel <alexandra@appticles.com>
2019-02-05 16:21:15 +0200

    Set Cassandra requests timeout

--- prediction/batch_inference/session_manager.py
+++ prediction/batch_inference/session_manager.py
@@ -12,11 +12,13 @@ class CassandraSessionManager:
         self.MORPHL_CASSANDRA_USERNAME = getenv('MORPHL_CASSANDRA_USERNAME')
         self.MORPHL_CASSANDRA_PASSWORD = getenv('MORPHL_CASSANDRA_PASSWORD')
         self.MORPHL_CASSANDRA_KEYSPACE = getenv('MORPHL_CASSANDRA_KEYSPACE')
+        self.CASS_REQ_TIMEOUT = 3600.0
 
         self.auth_provider = PlainTextAuthProvider(
             username=self.MORPHL_CASSANDRA_USERNAME, password=self.MORPHL_CASSANDRA_PASSWORD)
         self.cluster = Cluster(contact_points=[
-                               self.MORPHL_SERVER_IP_ADDRESS], auth_provider=self.auth_provider)
+                               self.MORPHL_SERVER_IP_ADDRESS], auth_provider=self.auth_provider,
+                               request_timeout=self.CASS_REQ_TIMEOUT)
 
         self.session = self.cluster.connect(self.MORPHL_CASSANDRA_KEYSPACE)
         self.session.row_factory = dict_factory
