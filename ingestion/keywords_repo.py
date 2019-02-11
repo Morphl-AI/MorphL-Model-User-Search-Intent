67529cf7e2b69a817bcee8b092565847d7e8274f
LaurentiuM <lau@fraudsolve.com>
2019-02-11 13:39:21 +0200

    DAG fixed imports

--- ingestion/keywords_repo.py
+++ ingestion/keywords_repo.py
@@ -3,10 +3,10 @@ from cassandra.query import BatchStatement
 
 class KeywordsByCSVRepo:
 
-    def __init__(self, session):
-        self.session = session
+	def __init__(self, session):
+		self.session = session
 
-        insert_statement = """
+		insert_statement = """
 		INSERT INTO usi_csv_features_raw_p (
 		csv_file_date,
 		group_id,
@@ -19,16 +19,15 @@ class KeywordsByCSVRepo:
 		VALUES (?, ?, ?, ?, ?, ?, ?)
 		"""
 
-        self.prep_insert_statement = session.prepare(insert_statement)
+		self.prep_insert_statement = session.prepare(insert_statement)
 
-    def insert(self, values):
-        print(values)
-        self.session.execute(self.prep_insert_statement,
-                             values, timeout=3600.0)
+	def insert(self, values):
+		print(values)
+		self.session.execute(self.prep_insert_statement, values, timeout=3600.0)
 
-    def batch_insert(self, batch_values):
-        batch = BatchStatement()
-        for val_set in batch_values:
-            batch.add(self.prep_insert_statement, val_set)
+	def batch_insert(self, batch_values):
+		batch = BatchStatement()
+		for val_set in batch_values:
+			batch.add(self.prep_insert_statement, val_set)
 
-        self.session.execute(batch, timeout=3600.0)
+		self.session.execute(batch, timeout=3600.0)
