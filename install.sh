478a774b7e289c93142f3213c195f183f7158c09
Alexandra Anghel <alexandra@appticles.com>
2019-02-11 15:33:10 +0200

    Fix typo

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ chown airflow /home/airflow/.morphl_usi_csv_environment.sh
 echo "export USI_GOOGLE_CLOUD_PROJECT=morphl-cloud" >> /home/airflow/.morphl_usi_csv_environment.sh
 echo "export USI_GOOGLE_CLOUD_BUCKET=usi-csv-samples" >> /home/airflow/.morphl_usi_csv_environment.sh
 echo "export USI_GOOGLE_CLOUD_PROCESSED=jot/processed" >> /home/airflow/.morphl_usi_csv_environment.sh
-echo "export USI_GOOGLE_CLOUD_UNPROCESSED=jot/processed" >> /home/airflow/.morphl_usi_csv_environment.sh
+echo "export USI_GOOGLE_CLOUD_UNPROCESSED=jot/unprocessed" >> /home/airflow/.morphl_usi_csv_environment.sh
 echo "export USI_GOOGLE_CLOUD_SERVICE_ACCOUNT=/opt/secrets/usi_csv/gcloud_service_account.json" >> /home/airflow/.morphl_usi_csv_environment.sh
 echo ". /home/airflow/.morphl_usi_csv_environment.sh" >> /home/airflow/.profile
 
