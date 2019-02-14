a6ba34c89c0217ae3a256eb97ed809622bc500d4
Alexandra Anghel <alexandra@appticles.com>
2019-02-14 14:51:26 +0200

    Fix typo

--- prediction/batch_inference/csv_files_repo.py
+++ prediction/batch_inference/csv_files_repo.py
@@ -8,7 +8,7 @@ class CSVFilesRepo:
         select_statement = "SELECT * from usi_csv_files WHERE always_zero = 0 AND is_processed = ? ALLOW FILTERING"
         self.prep_select_statement = session.prepare(select_statement)
 
-        update_statement = "'UPDATE usi_csv_files SET processed = ? WHERE always_zero = 0 AND day_of_data_capture = ?"
+        update_statement = "UPDATE usi_csv_files SET processed = ? WHERE always_zero = 0 AND day_of_data_capture = ?"
         self.prep_update_statement = session.prepare(update_statement)
 
     def select(self, values):
