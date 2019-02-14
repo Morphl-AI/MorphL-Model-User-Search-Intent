f85d2b3e3dda6b49f9e4e24b3775a269dc5aa9ae
Alexandra Anghel <alexandra@appticles.com>
2019-02-14 10:52:44 +0200

    Update csvs table when the ingestion pipeline is run, closes #6

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -3,38 +3,50 @@ import os
 from csv_processor import CsvProcessor
 from gs_manager import GoogleStorageManager
 from keywords_repo import KeywordsByCSVRepo
+from csv_files_repo import CSVFilesRepo
 from session_manager import CassandraSessionManager
 
 
 class DataIngestion:
 
-	def __init__(self):
-		self.session_manager = CassandraSessionManager()
-		self.keywords_repo = KeywordsByCSVRepo(self.session_manager.session)
-		self.storage_manager = GoogleStorageManager()
-		self.csv_processor = CsvProcessor()
+    def __init__(self):
+        self.session_manager = CassandraSessionManager()
+        self.keywords_repo = KeywordsByCSVRepo(self.session_manager.session)
+        self.csv_files_repo = CSVFilesRepo(self.session_manager.session)
+        self.storage_manager = GoogleStorageManager()
+        self.csv_processor = CsvProcessor()
 
-	def row_processor(self, csv_path):
-		filename_without_extension = os.path.splitext(os.path.basename(csv_path))[0]
-		return lambda values: [values.insert(0, self.csv_processor.format_date(filename_without_extension)), self.keywords_repo.insert(values)]
+    def get_csv_date(self, csv_path):
+        filename_without_extension = os.path.splitext(
+            os.path.basename(csv_path))[0]
+        return self.csv_processor.format_date(filename_without_extension)
 
-	def run(self):
-		for csv_path in self.storage_manager.get_unprocessed_filenames():
-			print('Ingesting ' + csv_path + "...")
+    def row_processor(self, csv_date):
+        return lambda values: [values.insert(0, csv_date), self.keywords_repo.insert(values)]
 
-			df = self.storage_manager.get_df(csv_path)
-			self.csv_processor.process_df(df, self.row_processor(csv_path))
-			self.storage_manager.move_to_processed(csv_path)
+    def file_processor(self, csv_date):
+        self.csv_files_repo.insert([csv_date])
 
-			print('Done with ' + csv_path)
+    def run(self):
+        for csv_path in self.storage_manager.get_unprocessed_filenames():
+            print('Ingesting ' + csv_path + "...")
 
-		self.session_manager.cluster.shutdown()
+            df = self.storage_manager.get_df(csv_path)
+            csv_date = self.get_csv_date(csv_path)
+
+            self.file_processor(csv_date)
+            self.csv_processor.process_df(df, self.row_processor(csv_date))
+            self.storage_manager.move_to_processed(csv_path)
+
+            print('Done with ' + csv_path)
+
+        self.session_manager.cluster.shutdown()
 
 
 def main():
-	data_ingestion = DataIngestion()
-	data_ingestion.run()
+    data_ingestion = DataIngestion()
+    data_ingestion.run()
 
 
 if __name__ == '__main__':
-	main()
+    main()
