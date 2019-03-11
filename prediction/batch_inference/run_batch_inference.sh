156bc7eb333ca2ad7d3cb2cf537424ef17f21c36
abarbulescu <alexandru@appticles.com>
2019-03-11 13:10:17 +0200

    Include all py files into spark submit

--- prediction/batch_inference/run_batch_inference.sh
+++ prediction/batch_inference/run_batch_inference.sh
@@ -1,5 +1,4 @@
 cp -r /opt/usi_csv /opt/code
 cd /opt/code
 git pull
-python /opt/code/prediction/batch_inference/usi_csv_batch_inference.py
-
+spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar --py-files /opt/code/prediction/batch_inference/embedding_manager.py,/opt/code/prediction/batch_inference/spark_session_manager.py,/opt/code/prediction/batch_inference/predictions_statistics_repo.py,/opt/code/prediction/batch_inference/session_manager.py,/opt/code/prediction/batch_inference/model_manager.py  /opt/code/prediction/batch_inference/usi_csv_batch_inference.py
