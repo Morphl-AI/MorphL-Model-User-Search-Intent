166ad7d71ff174b1aa895dcda7565a6b41b40c9f
abarbulescu <alexandru@appticles.com>
2019-03-18 13:06:25 +0200

    Clean up batch inference spark file imports

--- prediction/batch_inference/run_batch_inference.sh
+++ prediction/batch_inference/run_batch_inference.sh
@@ -1,4 +1,4 @@
 cp -r /opt/usi_csv /opt/code
 cd /opt/code
 git pull
-spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar --py-files /opt/code/prediction/batch_inference/embedding_manager.py,/opt/code/prediction/batch_inference/spark_session_manager.py,/opt/code/prediction/batch_inference/predictions_statistics_repo.py,/opt/code/prediction/batch_inference/session_manager.py,/opt/code/prediction/batch_inference/model_manager.py  /opt/code/prediction/batch_inference/usi_csv_batch_inference.py
+spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar  /opt/code/prediction/batch_inference/usi_csv_batch_inference.py
