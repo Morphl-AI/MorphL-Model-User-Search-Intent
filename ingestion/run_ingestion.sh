d2b8c466fd615ed04804cc74e518ad22026691ad
abarbulescu <alexandru@appticles.com>
2019-05-10 14:14:47 +0300

    Update dag and and bash commands for ingestion

--- ingestion/run_ingestion.sh
+++ ingestion/run_ingestion.sh
@@ -4,11 +4,19 @@ git pull
 
 
 GCP_PROJECT_ID=$(jq -r '.project_id' ${USI_GOOGLE_CLOUD_SERVICE_ACCOUNT})
+USI_LOCAL_PATH=/opt/landing/
 
 gcloud config set project ${USI_GOOGLE_CLOUD_PROJECT}
 
 gcloud auth activate-service-account --key-file=${USI_GOOGLE_CLOUD_SERVICE_ACCOUNT}
 
-gsutil cp -r dir gs://${USI_GOOGLE_CLOUD_BUCKET}/${USI_GOOGLE_CLOUD_UNPROCESSED} /opt/landing
-python /opt/code/ingestion/ingestion.py
+mkdir ${USI_LOCAL_PATH}
+
+gsutil cp -r dir gs://${USI_GOOGLE_CLOUD_BUCKET}/${USI_GOOGLE_CLOUD_UNPROCESSED} ${USI_LOCAL_PATH}
+
+export USI_LOCAL_PATH
+
+spark-submit --jars /opt/spark/jars/spark-cassandra-connector.jar,/opt/spark/jars/jsr166e.jar  /opt/code/ingestion/ingestion.py
+
+rm r- ${USI_LOCAL_PATH}
 
